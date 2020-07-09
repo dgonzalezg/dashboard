498b7075c5f0197a4349440778b87ffe23c9b9e0
dgonzalezg <dmgonzalezg@uc.cl>
2020-07-09 13:49:59 -0400

    Updates

---
+++ precache-manifest.96cc704caafd2a53d49c84f0b54b3537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a66ade5433999900122b4548f95ca4",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "9e00d9c96d038256b812",
+    "url": "/dashboard/static/css/main.05ee5307.chunk.css"
+  },
+  {
+    "revision": "96c711625d2c6ac37c29",
+    "url": "/dashboard/static/js/2.f1500f7c.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/dashboard/static/js/2.f1500f7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e00d9c96d038256b812",
+    "url": "/dashboard/static/js/main.27395e58.chunk.js"
+  },
+  {
+    "revision": "8f3ec870dce268db1d96",
+    "url": "/dashboard/static/js/runtime-main.2ec0fa51.js"
+  }
+]);
